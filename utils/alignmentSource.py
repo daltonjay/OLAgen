76cb0ce1886c994fbca4ddbcc0fd2fe9ccda7992
Dalton J. Nelson <59614908+daltonjay@users.noreply.github.com>
2024-04-29 15:31:37 -0500

    Add files via upload

--- utils/alignmentSource.py
+++ utils/alignmentSource.py
@@ -1,8 +1,9 @@
 # This module contains functions to perform (1) mafft and (2) clustalo alignments
-# For use aligning .fasta and GenBank Accession provided sequences as part of OLAgen
+# For use aligning .fasta provided sequences as part of OLAgen
 # Dalton J. Nelson, copyright 2024
 
 import subprocess
+import platform
 
 # def clustAlign(fastaCompiledFile): # Works as desired for a singular input file as of Jan 31, 2024
     
@@ -22,33 +23,49 @@ import subprocess
 #         # Handle any errors that occur during command execution
 #         print("Error executing ClustalO:", e)
 def clustAlign(fastaCompiledFile): # Works as desired for a singular input file as of Jan 31, 2024
-    
-    # Adjust the path to the clustalo.exe executable as needed
-    clustalo_cmd = ['alignments\\clustalo.exe', '-i', fastaCompiledFile, '-o', 'outClustalo.fasta', '--outfmt=fasta', '--force']
-
-    # Execute the ClustalO command
-    try:
-        # Run the ClustalO command
-        subprocess.run(clustalo_cmd, check=True)
-        print("ClustalO alignment completed successfully.")
-        return 1
-    except subprocess.CalledProcessError as e:
-        # Handle any errors that occur during command execution
-        print("Error executing ClustalO:", e)
-        return 0
+    system = platform.system()
+    if system == 'Darwin' or system == 'Linux':
+        # Adjust the path to the clustalo.exe executable as needed
+        clustalo_cmd = ['./utils/alignments/clustalo', '-i', fastaCompiledFile, '-o', 'outClustalo.fasta', '--outfmt=fasta', '--force']
+       
+        n = 0 
+        # Execute the ClustalO command
+        try:
+            # Run the ClustalO command
+            # Here, add a loading indicator rather than a progress bar
+            subprocess.run(clustalo_cmd, check=True)
+            print("ClustalO alignment completed successfully.")
+            n = 1
+            return n
+        except subprocess.CalledProcessError as e:
+            # Handle any errors that occur during command execution
+            print("Error executing ClustalO:", e)
+    else:
+        print('Unsupported operating system')
         
 #clustAlign('seqs_trial.fasta')
 
 def mafftAlign(fastaCompiledFile): # works as desired for a singular input file as of Jan 31, 2024
+    system = platform.system()
+    print(system)
+    
     try:
-        # Convert Windows path to WSL path
-        wsl_path = fastaCompiledFile.replace('\\', '/').replace('C:', '/mnt/c')
-
         # Construct the command
-        cmd = ['wsl.exe', 'bash', '-c', f"mafft --genafpair --out outMafft.fasta {wsl_path}"]
-
+        if system == 'Windows': 
+            # Convert Windows path to WSL path
+            wsl_path = fastaCompiledFile.replace('\\', '/').replace('C:', '/mnt/c')
+            
+            cmd = ['wsl.exe', 'bash', '-c', f"mafft --genafpair --out outMafft.fasta {wsl_path}"]
+        
+        if system == 'Darwin' or system == 'Linux':
+            cmd = ['mafft', '--genafpair', fastaCompiledFile]  # Command to execute
+        
+            with open('outMafft.fasta', 'w') as output_file:
+                # Execute the command and redirect output to the file
+                subprocess.run(cmd, stdout=output_file, check=True)
+        
         # Execute the command
-        subprocess.run(cmd, check=True)
+        #subprocess.run(cmd, check=True)
         print("Command executed successfully.")
     except subprocess.CalledProcessError as e:
         print(f"Error executing command: {e}")
